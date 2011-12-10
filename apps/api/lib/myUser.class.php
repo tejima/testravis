0c0f579790a51a626d8ba5d6a8ac246462bc588d
Kimura Youichi <kim.upsilon@bucyou.net>
2011-12-10 16:36:19 +0900

    add JSON API implementation (refs #2698)

--- apps/api/lib/myUser.class.php
+++ apps/api/lib/myUser.class.php
@@ -2,4 +2,60 @@
 
 class myUser extends sfBasicSecurityUser
 {
+  protected
+    $member = null;
+
+  public function getMemberByApiKey($apiKey)
+  {
+    if (!$apiKey)
+    {
+      return null;
+    }
+
+    $config = Doctrine::getTable('MemberConfig')->createQuery('c')
+      ->leftJoin('c.Member')
+      ->where('c.name = \'api_key\'')
+      ->where('c.value = ?', $apiKey)
+      ->fetchOne();
+
+    if (!$config)
+    {
+      return null;
+    }
+
+    return $config->getMember();
+  }
+
+  public function getMember()
+  {
+    if (null !== $this->member)
+    {
+      return $this->member;
+    }
+
+    $request = sfContext::getInstance()->getRequest();
+
+    $apiKey = $request['apiKey'];
+    if (false === $apiKey)
+    {
+      $exception = new opErrorHttpException('apiKey parameter not specified.');
+      throw $exception->setHttpStatusCode(401);
+    }
+
+    $member = $this->getMemberByApiKey($apiKey);
+    if (null === $member)
+    {
+      $exception = new opErrorHttpException('Invalid API key.');
+      throw $exception->setHttpStatusCode(401);
+    }
+
+    $this->member = $member;
+
+    return $member;
+  }
+
+  public function getMemberId()
+  {
+    return $this->getMember()->getId();
+  }
 }
--- apps/pc_frontend/templates/_layout.php
+++ apps/pc_frontend/templates/_layout.php
@@ -5,10 +5,18 @@
 <?php include_metas() ?>
 <?php include_title() ?>
 <?php include_stylesheets() ?>
-<?php include_javascripts() ?>
 <?php if (Doctrine::getTable('SnsConfig')->get('customizing_css')): ?>
 <link rel="stylesheet" type="text/css" href="<?php echo url_for('@customizing_css') ?>" />
 <?php endif; ?>
+<?php
+use_helper('Javascript');
+echo javascript_tag('
+var openpne = {
+  apiKey: "'.$sf_user->getMemberApiKey().'"
+};
+');
+?>
+<?php include_javascripts() ?>
 <?php echo $op_config->get('pc_html_head') ?>
 </head>
 <body id="<?php printf('page_%s_%s', $view->getModuleName(), $view->getActionName()) ?>" class="<?php echo opToolkit::isSecurePage() ? 'secure_page' : 'insecure_page' ?>">
