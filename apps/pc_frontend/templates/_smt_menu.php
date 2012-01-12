a2553d20ee9efbc33cd19ad4c6f3757992eabcd5
kashiwasan <pc.kashiwasan@gmail.com>
2012-01-12 15:45:41 +0900

    added global and local menu partial for smartphone  refs (#2227)

---
+++ apps/pc_frontend/templates/_smt_menu.php
@@ -0,0 +1,48 @@
+<div class="menuform hide template">
+  
+  <div class="menu-margin row">
+    <?php echo op_image_tag('MENU_MARGIN.png', array('width' => '324', 'height' => '12')); ?>
+  </div>
+  <div class="menu-top row">    　
+  </div>
+  <div class="menu-middle row">
+    <div class="span10 offset1 font20" style="text-align: left; margin-bottom: 4px;">
+     Home Menu
+    </div>
+  </div>
+  <div class="menu-middle row">
+    <div class="span11 offset1">
+      <a class="btn">Home</a>
+      <a class="btn">Diary</a>
+      <a class="btn">Community</a>
+
+      <a class="btn">Community</a>
+      <a class="btn">Logout</a>
+      <a class="btn">Home</a>
+      <a class="btn">Diary</a>
+      <a class="btn">Logout</a>
+      <a class="btn">Home</a>
+
+      <a class="btn">Diary</a>
+      <a class="btn">Community</a>
+      <a class="btn">Logout</a>
+    </div>
+  </div>
+  <div class="menu-middle row">
+    <div class="span10 offset1 font20" style="text-align: left; margin-bottom: 4px;">
+     Global Menu
+    </div>
+  </div>
+  <div class="menu-middle row">
+    <div class="span11 offset1">
+      <a class="btn">Home</a>
+      <a class="btn">Diary</a>
+      <a class="btn">Community</a>
+      <a class="btn">Logout</a>
+    </div>
+  </div>
+  <div class="menu-bottom row">
+    <div class="span12"></div>
+  </div>
+</div>
+
