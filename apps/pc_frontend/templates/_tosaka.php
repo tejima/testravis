4548fcf27c26aac9665c5b3787b02eaa938e1912
kashiwasan <pc.kashiwasan@gmail.com>
2012-01-17 18:55:05 +0900

    modified 'tosaka' for smartrphone (refs #2227)

--- apps/pc_frontend/templates/_tosaka.php
+++ apps/pc_frontend/templates/_tosaka.php
@@ -1,19 +1,151 @@
 <script type="text/javascript">
+//-----------------------------------
+//COMMENT BUTTON NAVIGATION
 $(document).ready(function(){
-  $("#postbutton").click(function(){
-    $(".postform").toggle();
+  $(".commentbutton").click(function(){
+    $(".commentform").toggle();
+    $(".commentbutton").toggle();
   });
 });
-$(document).ready(function(){
-  $("#menubutton").click(function(){
-    $(".menuform.template").toggle();
-  }); 
-});
 
+//-----------------------------------
+//TOSAKA BUTTON NAVIGATION
+$(document).ready(function(){
+  $(".postbutton").click(function(){
+    $(".toggle1:not(.postform)").hide();
+    $(".postform").toggle();
+    if($(".postform").is(":visible")){
+      $(".posttextarea").focus();
+    }
+  });
+
+  $(".ncbutton").click(function(){
+    $(".toggle1:not(.ncform)").hide();
+    $(".ncform").toggle();
+  });
+
+  $(".menubutton").click(function(){
+    $(".toggle1:not(.menuform)").hide();
+    $(".menuform").toggle();
+  });
+
+  $(".toggle1_close").click(function(){
+    $(".toggle1").hide();
+  });
+});
 </script>
 
-<div id="tosaka" class="row">
-  <div class="span4"><?php echo link_to(op_image_tag('LOGO.png', array('height' => '32')), '@homepage'); ?></div>
-  <div class="span4 center"><?php echo op_image_tag('NOTIFY_CENTER.png', array('height' => '32')) ?></div>
-  <div class="span3 offset1 center"><?php echo op_image_tag('POST.png', array('height' => '32')) ?></div>
+
+<!-- NCFORM TMPL -->
+<div class="ncform hide toggle1">
+  <hr class="toumei">
+  <div class="row">
+    <div class="span10 offset1 center white font14 toggle1_close">
+      通知センター
+      <hr class="toumei">
+    </div>
+    <div class="span1">
+      <?php echo op_image_tag('UPARROW', array('class' => 'toggle1_close')) ?>
+    </div>
+  </div>
+  <div class="row">
+  <hr class="toumei">
+  <hr class="toumei">
+    <div class="span12 center">
+      <img src="http://p.pne.jp/d/201201162250.png" alt="" />
+    </div>
+  </div>
+  <hr class="toumei">
+  <hr class="toumei">
+  <?php echo op_image_tag('SEPALATOR.png', array('height' => '6', 'width' => '320')) ?>
+</div>
+<!-- NCFORM TMPL -->
+
+
+<!-- MENUFORM TMPL -->
+<div class="menuform hide toggle1">
+  <hr class="toumei">
+  <div class="row">
+    <div class="span10 offset1 center white font14 toggle1_close">
+      MENU
+      <hr class="toumei">
+    </div>
+    <div class="span1">
+      <img class="toggle1_close" src="./UPARROW.png">
+      <?php echo op_image_tag('UPARROW', array('class' => 'toggle1_close')) ?>
+    </div>
+  </div>
+
+
+  <hr class="toumei">
+
+  <div class="menu-middle row">
+    <div class="span11 offset1">
+      <a class="btn">Home</a>
+      <a class="btn">Diary</a>
+      <a class="btn">Community</a>
+      <a class="btn">Community</a>
+      <a class="btn">Logout</a>
+      <a class="btn">Home</a>
+      <a class="btn">Diary</a>
+      <a class="btn">Logout</a>
+      <a class="btn">Home</a>
+      <a class="btn">Diary</a>
+      <a class="btn">Community</a>
+      <a class="btn">Logout</a>
+    </div>
+  </div>
+  <hr class="toumei">
+  <hr class="toumei">
+  <?php echo op_image_tag('SEPALATOR.png', array('height' => '6', 'width' => '320')) ?>
+</div>
+<!-- MENUFORM TMPL -->
+
+
+<!-- POSTFORM TMPL -->
+<div class="postform hide toggle1">
+  <hr class="toumei">
+  <div class="row">
+    <div class="span10 offset1 center white font14 toggle1_close">
+      投稿フォーム
+      <hr class="toumei">
+    </div>
+    <div class="span1">
+      <?php echo op_image_tag('UPARROW', array('class' => 'toggle1_close')) ?>
+    </div>
+  </div>
+  <div class="row">
+    <textarea class="posttextarea span12" rows="4" ></textarea>
+  </div>
+  <hr class="toumei">
+  <div class="row">
+    <button class="span10 offset1 btn small primary" >POST</button>
+  </div>
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <hr class="toumei">
+  <?php echo op_image_tag('SEPALATOR.png', array('height' => '6', 'width' => '320')) ?>
+</div>
+<!-- POSTFORM TMPL -->
+
+<div id="slot_tosaka">
+  <div class="row">
+    <div class="span12">
+      <div class="row">
+        <div class="span4"><?php echo link_to(op_image_tag('LOGO.png', array('height' => '32')), '@homepage'); ?></div>
+        <div class="span4 center"><?php echo op_image_tag('NOTIFY_CENTER.png', array('height' => '32', 'class' => 'ncbutton')) ?></div>
+        <div class="span3 offset1 center"><?php echo op_image_tag('POST.png', array('height' => '32', 'class' =>'postbutton')) ?></div>
+      </div>
+    </div>
+  </div>
 </div>
