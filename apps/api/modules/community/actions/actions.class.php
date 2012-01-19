90c76afcf90da74f6c0a6f86163a7df771fd4803
Kimura Youichi <kim.upsilon@bucyou.net>
2012-01-19 11:40:34 +0900

    add community/member.json (refs #2698)

--- apps/api/modules/community/actions/actions.class.php
+++ apps/api/modules/community/actions/actions.class.php
@@ -17,4 +17,27 @@ class communityActions extends opApiActions
 
     $this->setTemplate('array');
   }
+
+  public function executeMember(sfWebRequest $request)
+  {
+    if (isset($request['community_id']))
+    {
+      $communityId = $request['community_id'];
+    }
+    elseif (isset($request['id']))
+    {
+      $communityId = $request['id'];
+    }
+    else
+    {
+      $this->forward400('community_id parameter not specified.');
+    }
+
+    $this->members = Doctrine::getTable('Member')->createQuery('m')
+      ->addWhere('EXISTS (FROM CommunityMember cm WHERE m.id = cm.member_id AND cm.is_pre = false AND cm.community_id = ?)', $communityId)
+      ->limit(sfConfig::get('op_json_api_limit', 20))
+      ->execute();
+
+    $this->setTemplate('array', 'member');
+  }
 }
